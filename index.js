87690a1af0fd1872494a35df0a957d5e631f46ef
bydex <erok_erok@mail.ru>
2023-04-27 00:37:34 +0600

    added: cookie serializer

--- index.js
+++ index.js
@@ -4,73 +4,106 @@ const https = require("https");
 const fs = require("fs");
 const helmet = require("helmet");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 require("dotenv").config();
 
 const PORT = 3000;
 
+const config = {
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  cookieKey1: process.env.COOKIE_KEY_1,
+  cookieKey2: process.env.COOKIE_KEY_2,
+};
+
 const googleAuthConfig = {
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
-}
+  clientID: config.cookieKey1,
+  clientSecret: config.cookieKey2,
+  callbackURL: "/auth/google/callback",
+};
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-    console.log("Google profile", profile);
-    done(null, profile);
+  console.log("Google profile", profile);
+  done(null, profile);
 }
 
-passport.use(new GoogleStrategy(googleAuthConfig, verifyCallback))
+passport.use(new GoogleStrategy(googleAuthConfig, verifyCallback));
+
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 const app = express();
 
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.cookieKey1, config.cookieKey2],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(passport.initialize())
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
 
-function checkLoggedIn (req, res, next) {
-    const isLoggedIn = true;
-
-    if (!isLoggedIn) {
-        return res.status(401).json({
-            error: "You must log in!"
-        })
-    }
-    next();
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
 }
 
-app.get('/auth/google', passport.authenticate("google", {
-    scope: ["email"]
-}))
-app.get('/auth/google/callback',
-    passport.authenticate("google", {
-        failureRedirect: "/failure",
-        successRedirect: "/",
-        session: false
-    }),
-    (req, res) => {
-        console.log("Google called us back")
-    }
-)
-app.get('/auth/logout', (req, res) => {})
-
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: true,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
+app.get("/auth/logout", (req, res) => {});
 
 app.get("/failure", (req, res) => {
-    return res.send("Failed to log in!")
-})
+  return res.send("Failed to log in!");
+});
 
-app.get('/secret', checkLoggedIn, (req, res) => {
-    return res.send("Your personal secret value is 42!")
-})
+app.get("/secret", checkLoggedIn, (req, res) => {
+  return res.send("Your personal secret value is 42!");
+});
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-https.createServer({
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem")
-}, app).listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
